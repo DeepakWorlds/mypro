994f20cd4921e40f50f8d5961c0462c0e14d2576
MeDeepakS <4medeepaks@gmail.com>
2024-06-16 20:59:09 +0530

    added

---
+++ eventproject1/eventsapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect
+from . models import Event
+from . forms import BookingForm
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def events(request):
+    dict_eve={
+        'eve':Event.objects.all()
+    }
+    return render(request,'events.html',dict_eve)
+def booking(request):
+    if request.method=='POST':
+        form=BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'confirmation.html')
+            messages.success(request, 'Your booking has been successful!')
+            return redirect('/')
+
+    form=BookingForm()
+    dict_form={
+        'form':form
+    }
+    return render(request,'booking.html',dict_form)
+def contact(request):
+    return render(request,'contact.html')
